9c73121eeaca2122b9dba54e02375e4f8a753a21
Henriette Dusabimana <hdusabim@gmail.com>
2021-08-23 02:21:52 +0200

    initial commit for address book web app

---
+++ contacts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Contacts(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.fullname
+        
+
+    @property
+    def fullname(self):
+        return "%s %s" % (self.first_name, self.last_name)
---
+++ contacts/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from .models import Contacts
+
+# Create your views here.
+
+def index(request):
+    search_results =Contacts.objects.order_by('first_name')
+    contacts = Contacts.objects.order_by('first_name')
+    search_input = request.GET.get('search-area')
+    if search_input:
+        contacts = Contacts.objects.filter(first_name__icontains=search_input)
+        search_results = contacts
+        contacts = Contacts.objects.filter(last_name__icontains=search_input)
+        search_results = search_results | contacts
+    else:
+        contacts = Contacts.objects.order_by('first_name')
+        search_input = ''
+    return render(request, 'index.html', {'contacts': search_results, 'search_input': search_input})
+
+def addContact(request):
+    if request.method == 'POST':
+
+        new_contact = Contacts(
+            first_name=request.POST['first-name'],
+            last_name=request.POST['last-name'],
+            email=request.POST['email'],
+            phone_number=request.POST['phone-number'],
+            )
+        new_contact.save()
+        return redirect('/')
+
+    return render(request, 'new.html')
+
+def editContact(request, pk):
+    contact = Contacts.objects.get(id=pk)
+
+    if request.method == 'POST':
+        contact.first_name = request.POST['first-name']
+        contact.last_name = request.POST['last-name']
+        contact.phone_number = request.POST['phone-number']
+        contact.email = request.POST['email']
+        contact.save()
+
+        return redirect('/profile/'+str(contact.id))
+    return render(request, 'edit.html', {'contact': contact})
+
+def deleteContact(request, pk):
+    contact = Contacts.objects.get(id=pk)
+
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('/')
+
+    return render(request, 'delete.html', {'contact': contact})
+
+def contactProfile(request, pk):
+    contact = Contacts.objects.get(id=pk)
+    return render(request, 'contact-profile.html', {'contact':contact})
